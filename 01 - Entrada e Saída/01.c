dcf38f924ec5303f87947d764b445fd5de1089d7
gmendonca-cmd <gabrielmend.sousa@gmail.com>
2026-09-19 00:26:22 -0300

    feat: organizando arquivos

---
+++ 01 - Entrada e Saída/01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    float n1, n2, n3, media;
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &n1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%f", &n2);
+
+    printf("Digite a terceira nota: ");
+    scanf("%f", &n3);
+
+
+    media = n1 + n2 + n3;
+
+    printf("A média das 3 notas é: %f", media);
+
+
+    return 0;
+
+}
---
+++ Exercicios Decisao/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Compilado com sucesso no Linux!\n");
+    return 0;
+}
+
